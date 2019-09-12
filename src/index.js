388f5c353fc4af294f747ba35bb4a8eb9e9da039
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-09-12 11:08:57 -0500

    add movie-display changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  * es6 modules and imports
  */
@@ -14,17 +16,36 @@ const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+  console.log(movies);
+
+
+
+    $(document).ready(function () {
+      let htmlBuild = "";
+
+      movies.forEach(({title, rating, id}) => {
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+
+        htmlBuild += "<div id= movie-display>";
+        htmlBuild += "<h1> Title: </h1>";
+        htmlBuild += `<p> ${title} </p>`;
+        htmlBuild += "<h1> rating: </h1>";
+        htmlBuild += `<p>  ${rating} </p>`;
+        htmlBuild += "</div>";
+
+
+      });
+      $('#container').html(htmlBuild);
   });
+
+
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 }).then(() =>
 
     loading.style.display = 'none',
-
-
 
 ).then(() =>
 
